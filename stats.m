828d363b5504cb153ea3657118bcb4f3de3c19a1
Matthew J. Gerber <47827960+gerber211@users.noreply.github.com>
2021-02-11 09:13:32 -0800

    Update stats.m
    Added a a few line comments

--- stats.m
+++ stats.m
@@ -13,6 +13,7 @@ function stats(A)
 % Matrices -> vectors
 A = A(:);
 
+% Calculate statistics
 meana = mean(A);
 mediana = median(A);
 stda = std(A);
@@ -20,8 +21,10 @@ mina = min(A);
 maxa = max(A);
 rmsa = rms(A);
 
+% Just for simplified notation
 stats = [meana mediana stda mina maxa rmsa];
 
+% Output to command window the stats; using floating-point numbers
 fprintf('Mean: %f; Median: %f; STD: %f; Min: %f; Max: %f; RMS: %f\n', stats);
 
 end
